8b95f683c5d2f139a39979302ac3b583550f026f
ammarqassab <ammarqassab97@gmail.com>
2022-07-13 12:57:00 +0300

    add website

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const path = require('path')
-
-const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
-const withFonts = require('nextjs-fonts');
-const webpack = require("webpack");
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -19,14 +10,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-module.exports = withCSS( withSass( withImages( withFonts ({
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://shopingammar.c1.biz/:path*',
-            },
-        ]
-    },
-}))));
