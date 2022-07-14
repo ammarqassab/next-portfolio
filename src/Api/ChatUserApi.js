477a03af8b3561eaf8cf8b42f99fb1e9bca199b6
ammarqassab <ammarqassab97@gmail.com>
2022-07-14 12:11:53 +0300

    update

--- src/Api/ChatUserApi.js
+++ src/Api/ChatUserApi.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { apiurl, timeOut } from "./FormApi";
 
-axios.defaults.headers['Access-Control-Allow-Origin']='*'
-
 export const allMessageApi = (token, id) => {
     const service = axios.create({
         baseURL:apiurl,
