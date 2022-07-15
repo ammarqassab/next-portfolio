d78831b68d63b93af7f107cb1fbd20026d1687f1
ammarqassab <ammarqassab97@gmail.com>
2022-07-15 08:11:26 +0300

    update

--- src/Api/FormApi.js
+++ src/Api/FormApi.js
@@ -8,10 +8,10 @@ export const timeOut= 30000;
 
 // axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 // axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
-// axios.defaults.headers['Access-Control-Allow-Origin']='*'
-// axios.defaults.headers['Access-Control-Request']='*'
-// axios.defaults.headers['Access-Control-Allow-Methods']='GET, PUT, POST, DELETE, OPTIONS'
-// axios.defaults.headers['Access-Control-Allow-Headers']='Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With'
+axios.defaults.headers['Access-Control-Allow-Origin']='*'
+axios.defaults.headers['Access-Control-Request']='*'
+axios.defaults.headers['Access-Control-Allow-Methods']='GET, PUT, POST, DELETE, OPTIONS'
+axios.defaults.headers['Access-Control-Allow-Headers']='Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With'
 // axios.defaults.baseURL = 'http://shopingammar.c1.biz/api'
 // axios.defaults.headers.common['Authorization'] = 'Bearer '
 // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
