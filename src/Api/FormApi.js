333aee2b0901ac1c8fe3db8bfd5a1dc81f275e7e
ammarqassab <ammarqassab97@gmail.com>
2022-07-15 12:24:51 +0300

    update

--- src/Api/FormApi.js
+++ src/Api/FormApi.js
@@ -58,6 +58,19 @@ export const registeruserApi = (values) => {
 };
 
 export const loginuserApi = (values) => {
+
+    const auth = axios.create({
+        baseURL:'https://larecta99.000webhostapp.com',
+        timeout:timeOut,
+        headers:{
+            'X-Requested-With': 'XMLHttpRequest',
+        },
+        withCredentials: true,
+
+    });
+    
+    const csrf = auth.get('/sanctum/csrf-cookie')
+    
     const service = axios.create({
         baseURL:apiurl,
         timeout:timeOut,
@@ -88,7 +101,7 @@ export const logoutuserApi = (token) => {
 export const Middleware = (token) => {
 
     const auth = axios.create({
-        baseURL:apiurl,
+        baseURL:'https://larecta99.000webhostapp.com',
         timeout:timeOut,
         headers:{
             'X-Requested-With': 'XMLHttpRequest',
