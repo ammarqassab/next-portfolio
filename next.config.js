527cdb8e40ba73843cf60d225b5034eb277e382c
ammarqassab <ammarqassab97@gmail.com>
2022-07-16 16:13:34 +0300

    update

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'imgix',
-    path: 'https://larecta99.000webhostapp.com/api',
+    path: 'https://larect.000webhostapp.com/api',
   },
   env: {
     appName: 'Larect',
-    apiUrl:'https://larecta99.000webhostapp.com/api',
+    apiUrl:'https://larect.000webhostapp.com/api',
   },
   // basePath:'/larect',
   // assentPrefix:'/larect'
